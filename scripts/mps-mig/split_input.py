25de5f6f0ffb43bb3e38872d9029a9882c901c99
abgulhan <44143449+abgulhan@users.noreply.github.com>
2024-08-07 15:54:57 -0500

    Fix parallel execution bug and remove unused functions

--- scripts/mps-mig/split_input.py
+++ scripts/mps-mig/split_input.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 
+'''
+Split an input genome file into multiple files with size near goal_bp. Individual chromosomes are not split.
+
+Usage example:
+split_input.py --input <input_genome> --out ./blocked_20_chr_hg38 --to_2bit true --goal_bp 200000000 --max_chunks 20
+'''
+
 import argparse
 import collections
 import concurrent.futures
@@ -156,61 +163,6 @@ class FastaFile:
             self.sequences = self.sequences[: split_index - 1]
 
 
-def chunk_file(
-    target_fasta: FastaFile,
-    output_dir: str,
-    end_chr: str = "",
-    save_split: bool = False,
-    single_chr: bool = False,
-    chunk_size: int = TEN_MB,
-    overlap_size: int = TEN_KB,
-    debug: bool = False,
-) -> None:
-    sequence_list = [seq for seq in target_fasta]
-
-    title = sequence_list[0].description
-    assert ">" in title
-
-    if single_chr:
-        target_fasta.to_single_seq()
-
-    if end_chr:
-        target_fasta.discard_sequences_after_and_including(end_chr, debug)
-
-        if save_split:
-            save_split_file = f"{target_fasta.pathname}.to_{end_chr}.fa"
-
-            if debug:
-                print(
-                    f"Saving split file {target_fasta.pathname} to {save_split_file}",
-                    file=sys.stdout,
-                    flush=True,
-                )
-
-            with open(save_split_file, "w") as f:
-                for seq in target_fasta:
-                    print(f"{seq.description}", file=f)
-                    print(f"{seq.sequence}", file=f)
-
-
-#    # look into this re: overlap
-#    for line_index in range(0, len(data), chunk_size - overlap_size):
-#        end_line_index = min(len(data), line_index + chunk_size)
-#        block_file_name = os.path.join(output_dir, f"block_part{line_index}-{end_line_index}")
-#
-#        with open(block_file_name, "w") as out_file:
-#            to_write = data[line_index:end_line_index]
-#
-#            if ">" not in to_write[0]:
-#                out_file.write(title)
-#            out_file.writelines(to_write)
-#
-#            # find last sequence name for next file
-#            for line in reversed(to_write[:-overlap_size]):
-#                if ">" in line:
-#                    title = line
-#                    break
-
 
 def convert_to_2bit(root_dir: str) -> None:
     commands = []
@@ -318,7 +270,8 @@ def split_chr(
 
 
 def parallel_wrapper(pass_list: tuple[FastaFile, str, int, bool, bool]) -> list[int]:
-    return split_chr(*pass_list)
+    _pass_list = (FastaFile(pass_list[0]), pass_list[1], pass_list[2], pass_list[3])
+    return split_chr(*_pass_list)
 
 
 def mse(data: list[int], base: int) -> float:
@@ -336,39 +289,24 @@ if __name__ == "__main__":
         help="Input sequence in fasta or fasta.gz format",
     )
     parser.add_argument("--out", type=str, required=True, help="Output directory")
-
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument(
-        "--end_chr", default="", type=str, help="Use data up until this sequence"
-    )
-    group.add_argument(
-        "--single_chr",
-        action="store_true",
-        help="Use only first sequence name for all sequences",
+    parser.add_argument(
+        "--to_2bit", action="store_true", help="Convert partitioned inputs into .2bit format"
     )
 
-    parser.add_argument(
-        "--save_split_inputs", action="store_true", help="Save split input files"
-    )
-    parser.add_argument(
-        "--to_2bit", action="store_true", help="Convert data into .2bit format"
-    )
     parser.add_argument(
         "--max_chunks",
-        default=0,
+        default=20,
         type=int,
-        help="Calculate chunk size based on input size",
+        help="Maximum number of chunks to split input into. If --goal_bp is not provided, this is the exact number of bins to partition input into",
     )
-    parser.add_argument(
-        "--split_chr",
-        action="store_false",
-        help="Make each sequence a separate partition",
-    )
+    # TODO: could get rid of max_chunks parameter by checking whether local
+    # minima is enountered when calculating mse, when goal_bp is provided
+
     parser.add_argument(
         "--goal_bp",
         default=0,
         type=int,
-        help="goal basepairs across chunks, calculate best using MSE. Check up to --max_chunks number of bins",
+        help="Goal basepairs count for each partition. Number of partitions calculated using MSE. Check up to --max_chunks number of bins",
     )
     parser.add_argument("--debug", action="store_true", help="Print debug information")
 
@@ -379,83 +317,50 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     target_file = args.input
-    target_fasta = FastaFile(target_file, debug=True)
+    #target_fasta = FastaFile(target_file, debug=args.debug)
 
     target_block_dir = args.out
 
-    target_chunk_size = TEN_MB
-    query_chunk_size = TEN_MB
-    overlap_size = TEN_KB
+    if not os.path.exists(args.input):
+        print(f"Input file {args.input} does not exist.")
+        exit(1)
 
     os.makedirs(target_block_dir, exist_ok=True)
 
-    if args.max_chunks and not args.split_chr:
-        total_bases = target_fasta.total_bases
-        target_chunk_size = -(total_bases // -args.max_chunks)
-
-        if args.debug:
-            print(f"total_bases = {total_bases}", file=sys.stderr, flush=True)
-            print(
-                f"target chunk size = {target_chunk_size}", file=sys.stderr, flush=True
-            )
-
-    chr_end_target = args.end_chr.strip()
-
-    if args.split_chr:
+    if args.goal_bp:
         bin_count = args.max_chunks
         goal_bp = args.goal_bp
         best_bin_count = -1
         best_bin_loss = math.inf
-        prev_avg = 0
+        # NOTE: FastaFile object is not serializable and using it in pass list
+        # prevents parallel execution. As a workaround, pass the input file path
+        # and initialize FastFile object in parallel_wrapper
+        # TODO: find a better way to do this
+        pass_list = [
+            (target_file, "", i, False, False) for i in range(1, bin_count + 1)
+        ]
 
-        if args.goal_bp:
-            pass_list = [
-                (target_fasta, "", i, False, False) for i in range(1, bin_count + 1)
-            ]
+        cpus_available = len(os.sched_getaffinity(0))
+        if cpus_available > bin_count:
+            cpus_available = bin_count
 
-            cpus_available = len(os.sched_getaffinity(0))
-            if cpus_available > bin_count:
-                cpus_available = bin_count
+        if args.debug:
+            print(
+                f"DEBUG: spliting using {cpus_available} CPUs",
+                file=sys.stderr,
+                flush=True,
+            )
 
-            if args.debug:
-                print(
-                    f"DEBUG: spliting using {cpus_available} CPUs",
-                    file=sys.stderr,
-                    flush=True,
-                )
-
-            with concurrent.futures.ProcessPoolExecutor(
-                max_workers=cpus_available
-            ) as executor:
-                for bins in executor.map(parallel_wrapper, pass_list):
-                    i = len(bins)
-                    loss = mse(bins, goal_bp)
-
-                    if args.debug:
-                        print(
-                            f"DEBUG: * bin count {i}, mse {int(loss)}, bins {bins}",
-                            file=sys.stderr,
-                            flush=True,
-                        )
-
-                    if loss < best_bin_loss:
-                        best_bin_count = i
-                        best_bin_loss = loss
-        else:
-            for i in range(1, bin_count + 1):
-                bins = split_chr(
-                    target_fasta,
-                    target_block_dir,
-                    num_chunks=i,
-                    write_to_output_dir=False,
-                    debug=args.debug,
-                )
-                avg = sum(bins) / len(bins)
-                loss = abs(goal_bp - avg)
+        with concurrent.futures.ProcessPoolExecutor(
+            max_workers=cpus_available
+        ) as executor:
+            for bins in executor.map(parallel_wrapper, pass_list):
+                i = len(bins)
+                loss = mse(bins, goal_bp)
 
                 if args.debug:
                     print(
-                        f"DEBUG: bin count {i}, avg difference {int(loss)}",
+                        f"DEBUG: * bin count {i}, mse {int(loss)}, bins {bins}",
                         file=sys.stderr,
                         flush=True,
                     )
@@ -465,34 +370,29 @@ if __name__ == "__main__":
                     best_bin_loss = loss
 
         bin_count = best_bin_count
+    else:
+        bin_count = args.max_chunks
+    if args.debug:
 
-        if args.debug:
-            print(
-                f"DEBUG: bin_count = {bin_count}, loss={best_bin_loss}",
+        if args.goal_bp:
+            print( 
+                print(f"bin_count = {bin_count}, loss={best_bin_loss}"),
+                file=sys.stderr,
+                flush=True,
+            )
+        else:
+            print( 
+                f"DEBUG: bin_count = {bin_count}",
                 file=sys.stderr,
                 flush=True,
             )
 
-        split_chr(target_fasta, target_block_dir, bin_count, debug=args.debug)
-    else:
-        chunk_file(
-            target_fasta,
-            target_block_dir,
-            end_chr=chr_end_target,
-            save_split=args.save_split_inputs,
-            single_chr=args.single_chr,
-            chunk_size=target_chunk_size,
-            overlap_size=overlap_size,
-            debug=args.debug,
-        )
+    # TODO: this is a bit inefficient, since this was already calculated
+    # in parallel_wrapper (if goal_bp provided), but was not written to 
+    # output directory
+    target_fasta = FastaFile(target_file, debug=args.debug)
+    split_chr(target_fasta, target_block_dir, bin_count)
 
     if args.to_2bit:
         # Convert to 2bit format
         convert_to_2bit(target_block_dir)
-
-"""
-python split_input.py --input /home/mdl/abg6029/WGA_tests/inputs/hg38.fa --out /home/mdl/abg6029/WGA_tests/inputs/blocked_20_chr_hg38/ --to_2bit true --split_chr true --max_chunks 20
-
-python split_input.py --input hg38.fa --out ./blocked_20_chr_hg38 --to_2bit true --goal_bp 200000000 --max_chunks 20
-
-"""
