6cddc85f8ec793346c4a8a6b2b0839ddd576e277
Richard C. Burhans <rico@bx.psu.edu>
2024-08-27 18:42:17 -0400

    updates

--- README.md
+++ README.md
@@ -12,6 +12,10 @@ This is a [@galaxyproject](https://github.com/galaxyproject)'s modified fork of 
 
 - [Overview](#overview)
 - [Installation](#installation)
+- [Usage](#usage)
+  - [Alignment](#alignment)
+  - [Scoring Options](#scoring)
+  - [Output Options](#output)
 - [Citing SegAlign](#cite_segalign)
 
 ## <a name="overview"></a> Overview
@@ -24,8 +28,8 @@ Precise genome aligner efficiently leveraging GPUs.
 - Updated to compile with TBB (Threading Building Blocks) [version 2020.2](https://github.com/oneapi-src/oneTBB/releases/tag/v2020.2)
 - Fixed the --scoring option. It can now read and use the substitution matrix from a LASTZ [Scoring File](https://lastz.github.io/lastz/#fmt_scoring)
 - Added --num_threads option to limit the number of threads used
-- Added --segment_size option to limit maximum number of HSPs per segment file for CPU load balancing.
-- Added optional runner script using MIG and/or MPS for better GPU utilization.
+- Added --segment_size option to limit maximum number of HSPs per segment file for CPU load balancing
+- Added optional runner script using MIG and/or MPS for better GPU utilization
 
 ## <a name="installation"></a> Installation
 
@@ -33,6 +37,19 @@ For standalone installation use Conda: `conda install conda-forge::segalign`
 
 For installation in Galaxy we currently use the wrappers `richard-burhans:segalign` and `richard-burhans:batched_lastz` from the [Main Tool Shed](https://toolshed.g2.bx.psu.edu/).
 Try the tools at usegalaxy.org: [segalign](https://usegalaxy.org/root?tool_id=toolshed.g2.bx.psu.edu/repos/richard-burhans/segalign/segalign/), [batched_lastz](https://usegalaxy.org/root?tool_id=toolshed.g2.bx.psu.edu/repos/richard-burhans/batched_lastz/batched_lastz/)
+
+## <a name="usage"></a> Usage
+
+## <a name="alignment"></a> Alignment
+
+#### Running a Sample Alignment
+
+#### Running with MIG/MPS
+
+### <a name="scoring"></a>Scoring Options
+
+### <a name="output"></a>Output Options
+
 
 ## <a name="cite_segalign"></a> Citing SegAlign
 
---
+++ scripts/make-conda-env.bash
@@ -0,0 +1,102 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+project_root="."
+conda_env_file="conda-env.bash"
+conda_env_dev_file="conda-env-dev.bash"
+conda_root="$project_root/.conda"
+
+test -e "$conda_root" || mkdir -p "$conda_root"
+
+dev=0
+if [ $# -eq 1 ]; then
+    if [ "$1" = "dev" -o  "$1" = "-dev" -o "$1" = "--dev" ]; then
+        dev=1
+    fi
+fi
+
+##
+## download the latest Miniforge
+##
+
+miniforge_root="$conda_root/miniforge3"
+if [ ! -e "$miniforge_root" ]; then
+    repo="conda-forge/miniforge"
+    tag=$(curl --silent "https://api.github.com/repos/$repo/releases/latest" | jq -r ".tag_name // empty")
+    if [ -z "$tag" ]; then
+        echo "unable to get latest tag for $repo"
+        exit 1
+    fi
+    filename="Miniforge3-${tag}-$(uname -s)-$(uname -m).sh"
+    url="https://github.com/$repo/releases/download/$tag/$filename"
+
+    curl --silent --location -o "$conda_root/$filename" "$url"
+    curl --silent --location -o "$conda_root/${filename}.sha256" "${url}.sha256"
+    expected_checksum=$(awk '{print $1}' "$conda_root/${filename}.sha256")
+    actual_checksum=$(openssl dgst -sha256 "$conda_root/$filename" | awk '{print $2}')
+
+    if [ "$expected_checksum" != "$actual_checksum" ]; then
+        echo "Checksum mismatch: $conda_root/$filename"
+        exit 1
+    fi
+
+    ##
+    ## install Miniforge
+    ##
+
+    bash "$conda_root/$filename" -b -p "$miniforge_root"
+    rm -f "$conda_root/$filename" "$conda_root/${filename}.sha256"
+fi
+
+if [ $dev -eq 0 ]; then
+    echo "source \"${miniforge_root}/etc/profile.d/conda.sh\"" > "$conda_env_file"
+    echo "source \"${miniforge_root}/etc/profile.d/mamba.sh\"" >> "$conda_env_file"
+    source "$conda_env_file"
+    echo "conda activate segalign" >> "$conda_env_file"
+
+    installed=$(conda env list | grep -E "^segalign\b" || true)
+    if [ -z "$installed" ]; then
+        mamba create \
+            --name segalign \
+            --channel conda-forge \
+            --channel bioconda \
+            --channel defaults \
+            --override-channels \
+            --strict-channel-priority \
+            --yes \
+            "segalign-galaxy" \
+            "nvidia-ml-py"
+    fi
+elif [ $dev -eq 1 ]; then
+    echo "source \"${miniforge_root}/etc/profile.d/conda.sh\"" > "$conda_env_dev_file"
+    echo "source \"${miniforge_root}/etc/profile.d/mamba.sh\"" >> "$conda_env_dev_file"
+    source "$conda_env_dev_file"
+    echo "conda activate segalign-dev" >> "$conda_env_dev_file"
+
+    installed=$(conda env list | grep -E "^segalign-dev\b" || true)
+    if [ -z "$installed" ]; then
+        mamba create \
+            --name segalign-dev \
+            --channel conda-forge \
+            --channel bioconda \
+            --channel defaults \
+            --override-channels \
+            --strict-channel-priority \
+            --yes \
+            "black" \
+            "cmake" \
+            "flake8" \
+            "gxx=11" \
+            "libboost-devel>=1.70" \
+            "mypy" \
+            "nvidia-ml-py" \
+            "python=3.12" \
+            "tbb-devel=2020.2.*" \
+            "zlib"
+
+    fi
+fi
+
